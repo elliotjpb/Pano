3d3605529efe4436fb01bdc722246377af499d24
Elliot Brown <elliotjpb@gmail.com>
2018-06-19 21:03:04 +0100

    initial commit

---
+++ panoMat.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace cv::xfeatures2d;
+
+void readme();
+
+/** @function main */
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        readme(); return -1;
+    }
+
+    Mat I_1 = imread(argv[1], IMREAD_GRAYSCALE);
+    Mat I_2 = imread(argv[2], IMREAD_GRAYSCALE);
+
+    if (!I_1.data || !I_2.data)
+    {
+        std::cout << " --(!) Error reading images " << std::endl; return -1;
+    }
+
+    //-- Step 1: Detect the keypoints using SURF Detector
+    int minHessian = 400;
+
+    Ptr<SURF> detector = SURF::create(minHessian);
+
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+    detector->detect(I_1, keypoints_object);
+    detector->detect(I_2, keypoints_scene);
+
+    //-- Step 2: Calculate descriptors (feature vectors)
+    Ptr<SURF> extractor = SURF::create();
+    //SurfDescriptorExtractor extractor;
+
+    Mat descriptors_object, descriptors_scene;
+
+    extractor->compute(I_1, keypoints_object, descriptors_object);
+    extractor->compute(I_2, keypoints_scene, descriptors_scene);
+
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match(descriptors_object, descriptors_scene, matches);
+
+    double max_dist = 0; double min_dist = 100;
+
+    //-- Quick calculation of max and min distances between keypoints
+    for (int i = 0; i < descriptors_object.rows; i++)
+    {
+        double dist = matches[i].distance;
+        if (dist < min_dist) min_dist = dist;
+        if (dist > max_dist) max_dist = dist;
+    }
+
+    printf("-- Max dist : %f \n", max_dist);
+    printf("-- Min dist : %f \n", min_dist);
+
+    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+    std::vector< DMatch > good_matches;
+
+    for (int i = 0; i < descriptors_object.rows; i++)
+    {
+        if (matches[i].distance < 3 * min_dist)
+        {
+            good_matches.push_back(matches[i]);
+        }
+    }
+
+    Mat img_matches;
+    drawMatches(I_1, keypoints_object, I_2, keypoints_scene,
+                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+
+    for (int i = 0; i < good_matches.size(); i++)
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
+        scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
+    }
+
+    Mat H_12 = findHomography(obj, scene, RANSAC);
+
+    std::cout << H_12 << '\n';
+
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = cvPoint(0, 0); obj_corners[1] = cvPoint(I_1.cols, 0);
+    obj_corners[2] = cvPoint(I_1.cols, I_1.rows); obj_corners[3] = cvPoint(0, I_1.rows);
+    std::vector<Point2f> scene_corners(4);
+
+    //perspectiveTransform(obj_corners, scene_corners, H_12);
+    //-- Show detected matches
+
+
+    //Have 2 images. I_1, I_2. Need to transofm to the other with Homography H_12.
+    //Now creating blnak matrix twice size of images.
+    //alternative with homogenious coordinates
+    //Mat canvas(720, 540, 1, Scalar(0));
+
+    Mat blankcanvas = Mat::zeros(720,540,CV_8UC1);
+
+    imshow("Pano Homography", img_matches);
+
+    waitKey(0);
+    return 0;
+}
+
+/** @function readme */
+void readme()
+{
+    std::cout << " Usage: ./SURF_descriptor <img1> <img2>" << std::endl;
+}
