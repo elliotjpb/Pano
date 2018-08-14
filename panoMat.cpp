c3f611e3bca8a7d8ca67ef9bfab2df07b1e77057
Elliot Brown <elliotjpb@gmail.com>
2018-08-14 16:43:30 +0100

    video but only first frame loaded

--- panoMat.cpp
+++ panoMat.cpp
@@ -90,118 +90,35 @@ void readme(){
 /** @function main */
 int main(int argc, char** argv){
 
-    if (argc != 3){
-        readme(); return -1;
-    }
+  // Create a VideoCapture object and open the input file
+  // If the input is the web camera, pass 0 instead of the video file name
+  VideoCapture cap1("left.mov");
+  VideoCapture cap2("right.mov");
 
-    Mat image1 = imread(argv[1], IMREAD_COLOR);
-    Mat image2 = imread(argv[2], IMREAD_COLOR);
+  // Check if camera opened successfully
+  if(!cap1.isOpened() || !cap2.isOpened()){
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
 
-    // Mat image1 = imread(argv[1], IMREAD_GRAYSCALE);
-    // Mat image2 = imread(argv[2], IMREAD_GRAYSCALE);
+    //int loop=0;
+    while(1){
 
-    if (!image1.data || !image2.data){
-        std::cout << " --(!) Error reading images " << std::endl; return -1;
-    }
+    Mat cap1frame;
+    Mat cap2frame;
+    // Capture frame-by-frame
+    cap1 >> cap1frame;
+    cap2 >> cap2frame;
 
-    imshow( "Result", Stitching(image1,image2));
+    // If the frame is empty, break immediately
+    if (cap1frame.empty() || cap2frame.empty())
+      break;
+
+    imshow( "Result", Stitching(cap1frame,cap2frame));
 
     destroyWindow("Stitching");
 
     waitKey(0);
     return 0;
+  }
 }
-
-//imshow( "Result", trans_mat);
-
-
-//    while(1) {
-//        frame = cvQueryFrame(capture);
-//        if(loop>0){
-//            if(!frame) break;
-//
-//            image2=Mat(frame, false);
-//            result=Stitching(image1,image2);
-//            before_frame=result;
-//            frame=&before_frame;
-//            image1=result;
-//            image2.release();
-//            //imshow("Stitching",frame);
-//            cvShowImage("Stitching",frame);
-//            //break;
-//
-//        }else if(loop==0){
-//            //Mat aimage1(frame);
-//            image1=Mat(frame, false);
-//        }
-//        loop++;
-//        char c = cvWaitKey(33);
-//        if(c==27) break;
-//    }
-//
-
-//perspectiveTransform(obj_corners, scene_corners, H_12);
-//-- Show detected matches
-
-
-//Have 2 images. I_1, I_2. Need to transofm to the other with Homography H_12.
-//Now creating blnak matrix twice size of images.
-//alternative with homogenious coordinates
-//Mat canvas(720, 540, 1, Scalar(0));
-//This is for combining images I_1, I_2.
-
-
-//Mat V_12 = Mat::zeros(720,540,CV_8UC1);
-
-//cv::warpPerspective(I_2, V_12, H_12, V_12.size( ));
-
-//Now you have four canvases, all of which are the width of the 4 combined images,
-//and with one of the images transformed into the relevant place on each.
-
-//NOW -- merge the transformed images onto eachother. With ROI
-/*
-selecting the rectangle region of interest inside the image and cut or
-display part of the image from the bigger picture.
-*/
-
-//set the left side to white size - 360x270.
-//Mat M_1 = Mat::zeros(270,360,CV_8UC1);
-//M_1.setTo(cv::Scalar(255,255,255));
-
-//Mat M_2 = Mat::zeros(270,360,CV_8UC1);
-
-
-//cv::warpPerspective(M_1, M_2, H_12, M_1.size( ));
-
-
-//cv::Mat pano = Mat::zeros(M_1.size( ), CV_8UC3);
-
-// I_1.copyTo(pano, M_1);
-// V_12.copyTo(pano, M_2);
-//
-
-// imshow("Pano Homography", pano);
-
-//Need to define M_1...
-//--To later bring it all into the Pano
-// cv::Mat pano = zeros(M_1.size( ), CV_8UC3);
-// I_1.copyTo(pano, M_1);
-// V_12.copyTo(pano, M_2);
-
-//imshow("Pano Homography", img_matches);
-
-//--Compile
-/*
-
-g++ panoMat.cpp -I"/usr/local/Cellar/opencv/3.4.1_5/include" -L"/usr/local/Cellar/opencv/3.4.1_5/lib/" -I/u
-sr/local/include/opencv -I/usr/local/include -L/usr/local/lib -lopencv_stitching -lopencv_superres -lopencv_videostab -lopencv_aruco -lopencv_bgsegm -
-lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dpm -lopencv_face -lopencv_photo -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lo
-pencv_line_descriptor -lopencv_optflow -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrappin
-g -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_dnn -lopencv_plot -lopencv_xfeatures2d -lopencv_shape -lopencv_video -lopencv
-_ml -lopencv_ximgproc -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_flann -lopencv_xobjdetect -lopencv_imgcodecs -lo
-pencv_objdetect -lopencv_xphoto -lopencv_imgproc -lopencv_core -o panoMat
-
-*/
-
-// --Run
-// ./panoMat IMG_0856-2.png IMG_0857-2.png
